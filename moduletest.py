cd8e075a83c8c118648c5f9ebdbad046d6e6bde8
NitroxKnight <gerben.beintema@gmail.com>
2015-03-08 19:47:41 +0100

    general testing doc v1
    use most of the functions in here see if everthing is able to get true

--- moduletest.py
+++ moduletest.py
@@ -1,2 +1,26 @@
 
+
 import polypy
+po = polypy
+
+a = po.Poly([1])
+for i in range(6):
+	a *= po.Poly([i,1])
+z = po.fzeros(a)
+m = a(z)
+if sum([abs(i) for i in m])>10**-6:
+	print('001 zerofinder')
+b = -a
+a += 1
+a += a
+a *= 1
+a *= a
+a -= a
+a -= 1
+a //= po.Poly([1,1])
+a **= 2
+a >>= 2
+a <<= 2
+b = po.I(po.D(a))
+if b.macht!=a.macht:
+	print('002 integral en dif')
