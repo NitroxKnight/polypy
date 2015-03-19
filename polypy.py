08d688d7c60e94df8d3f3679cd455b37485b209a
NitroxKnight <gerben.beintema@gmail.com>
2015-03-19 21:40:21 +0100

    doc update

--- polypy.py
+++ polypy.py
@@ -1,11 +1,13 @@
-# a0+a1*x+a2*x^2+a3*x^3+a4*x^4
+
 
 ## to do:
-# make __doc__
 # https://docs.python.org/3/reference/datamodel.html#customizing-class-creation (type updatae)
 # error raiseing: https://docs.python.org/3/reference/simple_stmts.html#raise
+# make equallength more effective
 
-'''# polypy
+'''
+
+# polypy
 This module was made to do polynomial calculus with python. Made for python 3
 
 features are:
@@ -32,13 +34,19 @@ to Integrate,
 to find zeros, 
 	zerolist = polypy.fzeros(foo,startx=0,acc=10**-10)
 to find a single zeros (most property the closest to the startx):
-	singlezero = fsinglezero(foo,startx,acc=10**-14,maxinter=200)'''
+	singlezero = fsinglezero(foo,startx,acc=10**-14,maxinter=200)
+	
+'''
 
 
 class poly():
-	'''a polynomial is defind as a0 + a1*x + a2*x^2 + a3*x^3 + a4*x^4 .... an*x^n
+	'''
+	
+	a polynomial is defind as a0 + a1*x + a2*x^2 + a3*x^3 + a4*x^4 .... an*x^n
 	p = poly([a0,a1,a2,.....])
-	to calculate a point, p(x)'''
+	to calculate a point, p(x)
+	
+	'''
 	def __init__(self,coefficients): #create
 		if type(coefficients)==int or type(coefficients)==float:
 			self.terms = [coefficients]
@@ -47,13 +55,13 @@ class poly():
 	##doc and str and print
 	def __repr__(self):
 		return self.__str__()
-	def __call__(self,waarde): #bereken 
-		if type(waarde)==list or type(waarde)==range:
+	def __call__(self,x): #bereken 
+		if type(x)==list or type(x)==range:
 			m = []
-			for j in waarde: m.append(sum([self.terms[i]*j**i for i in range(len(self))]))
+			for j in x: m.append(sum([self.terms[i]*j**i for i in range(len(self))]))
 			return m
 		else:
-			return sum([self.terms[i]*waarde**i for i in range(len(self))])
+			return sum([self.terms[i]*x**i for i in range(len(self))])
 	def __str__(self): #print
 		r = 10 #round
 		if len(self)==0:
@@ -94,6 +102,10 @@ class poly():
 
 	##tools:
 	def rzero(self): #remove extra zero on the end of a polynomial
+		'''
+		removal of the end zeros of the p.data array
+		1+x+0*x**2+0*x**3 --> 1+x
+		'''
 		if sum([abs(i) for i in self.terms]) != 0:
 			while self.terms[-1]==0:
 				self.terms.pop(-1)
@@ -101,6 +113,11 @@ class poly():
 		else:
 			return poly([0])
 	def equallength(self,powers):
+		'''
+		
+		returns 2 polynomial of the same length
+		
+		'''
 		a = max(len(self),len(powers.terms))
 		b = [0 for i in range(a)]
 		c = [0 for i in range(a)]
@@ -112,11 +129,16 @@ class poly():
 	
 	##len
 	def __len__(self): #len
+		'''
+		return the amount of terms (that is equal to the highst power+1)
+		len(1+x) --> 2 
+		len(x+x**2) --> 3
+		'''
 		return len(self.terms)
 	
 	##math
 	# adding
-	def __pos__(self):
+	def __pos__(self): #+a
 		return self	
 	def __add__(self,powers):#a+b
 		if type(powers)==int or type(powers)==float:
@@ -149,6 +171,10 @@ class poly():
 		return self-powers
 	#multiply
 	def __mul__(self,powers):
+		'''
+		returns the multiple of 2 polynomials (also works with int and float)
+		
+		'''		
 		if type(powers)==int or type(powers)==float:
 			return poly([i*powers for i in self.terms])
 		else:
@@ -163,7 +189,19 @@ class poly():
 	def __imul__(self,powers):
 		return self*powers
 	#divide	
-	def __truediv__(self,powers): #powersnoom,rest
+	def __truediv__(self,powers): #polynomial,remainder
+		'''
+		If the denominator is an int or a foat:
+			all the terms will get diveded by the int/foat
+		
+		If the denominator is an poly:
+			more info on: http://en.wikipedia.org/wiki/Polynomial#Divisibility
+			self/powers == polynomial+remainder/powers --> self == powers*polynomial+remainder
+			This finds the polynomial and remainder suchs that this is true.
+			returns the (polynomial,remainder)
+			note: both polynomial and remainder is a polynomial
+		
+		'''
 		if type(powers)==int or type(powers)==float:
 			return poly([i/powers for i in self.terms])
 		else:
@@ -181,19 +219,33 @@ class poly():
 	def __itruediv__(self,powers):
 		return self/powers
 	def __floordiv__(self,powers):
+		'''
+		return just the polynomial in self/powers
+		'''
 		p,r = self/powers
 		return p
 	def __ifloordiv__(self,powers):
 		return self//powers
 	def __mod__(self,powers):
+		'''
+		returns just the remainder of self/powers
+		'''
 		p,r = self/powers
 		return r
 	def __imod__(self,powers):
 		return self%powers
 	def __divmod__(self,powers):
+		'''
+		same as self/powers
+		'''
 		return self/powers
 	#a>>2 is the same as a*x**2 and a<<2 is the same as (a/x**2)//1
 	def __rshift__(self,g):
+		'''
+		this is no real mathematical operation but here it is defind as:
+			self>>2==self*x**2
+			self<<2==self//x**2 (//==floordiv)
+		'''
 		if g>0:
 			a = [0 for i in range(g+len(self))]
 			for i in range(len(self)): a[i+g]=self.terms[i]
@@ -207,12 +259,20 @@ class poly():
 	def __irshift__(self,g):
 		return self>>g
 	def __lshift__(self,g):
+		'''
+		this is no real mathematical operation but here it is defind as:
+			self>>2==self*x**2
+			self<<2==self//x**2 (//==floordiv)
+		'''
 		return self>>-g
 	def __ilshift__(self,g):
 		return self<<g
 	#powers**int
 	def __pow__(self,p): 
-		#error handeling
+		'''
+		returns the self**p==self*self**(p-1)
+		with self**0==1
+		'''
 		if p==0:
 			return poly([0])
 		elif p == 1:
@@ -226,12 +286,22 @@ class poly():
 		return self**p
 
 def D(powers):
+	'''
+	gives the d/dx*powers
+	'''
 	return poly([i*p for i,p in zip(range(1,len(powers)),(powers<<1).terms)])
 
 def I(powers):
+	'''
+	give the integral of a polynomial
+	with c=0
+	'''
 	return poly([p/i for i,p in zip([1]+list(range(1,len(powers)+1)),(powers>>1).terms)])
 
 def fsinglezero(powers,startx,acc=10**-14,maxinter=200):
+	'''
+	finds a root of the polynomial close to the startx
+	'''
 	z = []
 	af = D(powers)
 	x = startx
@@ -242,33 +312,37 @@ def fsinglezero(powers,startx,acc=10**-14,maxinter=200):
 		xoud = x
 		t = af(x)
 		if t==0:
-			z = 'deelnul'
+			z = 'flat'
 			i = maxinter
 		else:
 			x -= powers(x)/t
 		dx = abs(xoud-x)
 	if i==maxinter:
-		if z == 'deelnul':
-			return 'deelnul'
+		if z == 'flat':
+			return 'flat'
 		else:
-			return 'maxinter'
+			return 'no zeros found'
 	else:
 		return x
 
 def fzeros(powers,startx=0,acc=10**-10):
-	'''This function gives all[1] the real roots (poly=0) for a given polynomial
+	'''
 	
-	[1] if the function is not to complex it will find all the roots
-	example: 0.1+x**2-(10**-10)*x**4'''
+	This function gives all* the real roots (poly=0) for a given polynomial
+	
+	* if the function is not to complex it will find all the roots
+	example: 0.1+x**2-(10**-10)*x**4
+	
+	'''
 	p = []
 	workworkpowers = poly(powers.terms[:]).rzero()
 	control = 0
 	m = startx
 	while True:
 		m = fsinglezero(workworkpowers,m,acc,50)
-		if m=='maxinter':
+		if m=='no zeros found':
 			return p
-		elif m=='deelnul':
+		elif m=='flat':
 			m = control
 			control+=1
 			if control == 5:
@@ -278,6 +352,10 @@ def fzeros(powers,startx=0,acc=10**-10):
 			workworkpowers //= poly([-m,1])
 
 def ptopoly(x,y):
+	'''
+	genartes a polynomial through the x and y point given
+	More info how this is done: http://en.wikipedia.org/wiki/Lagrange_polynomial
+	'''
 	def l(x,i):
 		#t/m
 		t = poly([1])
